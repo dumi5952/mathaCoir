e531ea4cfe433cc8e122ce1841aac7cc751c45e3
dumi5952 <60010354+dumi5952@users.noreply.github.com>
2020-10-10 23:03:56 +0530

    template

---
+++ view/mainCategoryTemp.php
@@ -0,0 +1,42 @@
+<?php include_once('includes/header.php')?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Matha Coir</title>
+
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/custom-css/main.css">
+    <link rel="stylesheet" href="../css/custom-css/footer.css">
+    <link rel="stylesheet"  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <div class="container-fluid category-name-container">
+        <h1>Category Name</h1>
+    </div>
+
+    <div>
+        <p>
+            category description
+        </p>
+    </div>
+    <hr>
+    <div>
+    <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="../img/1.jpg" alt="Card image cap">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+    </div>
+<br><br>
+
+<?php include_once('includes/footer.php')?>
+<script src="../js/jquery.slim.min.js"></script>
+<script src="../js/bootstrap.bundle.min.js"></script>
+<script src="../js/fontawesome.min.js"></script>
+</body>
+</html>
