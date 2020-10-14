4bcab25d4f81560364c252d50703f12f8004ed88
osinduw <55203995+osinduw@users.noreply.github.com>
2020-10-14 21:39:58 +0530

    caurasal

--- view/index.php
+++ view/index.php
@@ -11,6 +11,36 @@
     <link rel="stylesheet"  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="../img/1. Got Root Wallpaper my Pcbots WideScreen HD.png" height="500" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="../img/4. Spammer Forever by pcbots.png" height="500" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="../img/5. Bots Army Wallpaper.png" height="500" alt="Third slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="../img/6. White Cat hacker wallpaper.png" height="500" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
 hi
 <?php include_once('includes/footer.php')?>
 <script src="../js/jquery.slim.min.js"></script>
